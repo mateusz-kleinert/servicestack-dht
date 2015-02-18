c92f7a8f02cedd8b966adda081965a5a4d8777e1
mateusz-kleinert <mateusz.kleinert@gmail.com>
2015-02-18 15:42:39 +0100

    fixes

--- servstack/src/HashFunctions.cs
+++ servstack/src/HashFunctions.cs
@@ -4,16 +4,15 @@ namespace servstack
 {
 	public class HashFunctions
 	{
-		public static int Prime = 1327;
 		public static int h1 (String key)
 		{
-			var hash = key.Length * Prime;
+			var hash = key.GetHashCode () > 0 ? key.GetHashCode () : -key.GetHashCode ();
 			return hash % (Hardcode.PrimaryUpperBound - Hardcode.PrimaryLowerBound + 1) + Hardcode.PrimaryLowerBound;
 		}
 
 		public static int h2 (String key)
 		{
-			var hash = key.Length * Prime;
+			var hash = key.GetHashCode () > 0 ? key.GetHashCode () : -key.GetHashCode ();
 			return hash % (Hardcode.SecondaryUpperBound - Hardcode.SecondaryLowerBound + 1) + Hardcode.SecondaryLowerBound;
 		}
 	}
