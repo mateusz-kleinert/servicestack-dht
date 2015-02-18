97e095d986e837c98562aff5d5856cdf64ba05cb
Pawel Lampe <scony@sconysoft.com>
2015-02-18 19:36:05 +0100

    fix

--- servstack/src/HashFunctions.cs
+++ servstack/src/HashFunctions.cs
@@ -7,13 +7,11 @@ namespace servstack
 		public static int h1 (String key)
 		{
 			var hash = Math.Abs(key.GetHashCode() * 1327);
-			//var hash = key.Length * 1327;
 			return hash % (Hardcode.PrimaryUpperBound - Hardcode.PrimaryLowerBound + 1) + Hardcode.PrimaryLowerBound;
 		}
 
 		public static int h2 (String key)
 		{
-			//var hash = key.GetHashCode() * 7919;
 			var hash = Math.Abs(key.Length * 7919);
 			return hash % (Hardcode.SecondaryUpperBound - Hardcode.SecondaryLowerBound + 1) + Hardcode.SecondaryLowerBound;
 		}
