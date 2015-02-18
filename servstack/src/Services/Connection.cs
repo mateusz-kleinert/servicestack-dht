3748f1759949c08b25f77e8cf28bd6f118a012e0
Mateusz Kleinert <mateusz.kleinert@student.put.poznan.pl>
2015-02-18 08:14:53 +0100

    Connection fix

--- servstack/src/Services/Connection.cs
+++ servstack/src/Services/Connection.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System;
 using ServiceStack.Text;
 using servstack;
+using server;
+using System.Net;
 
 namespace server.Services
 {
@@ -22,8 +24,14 @@ namespace server.Services
 
 	public class ConnectionService: Service
 	{
-		public object Any(Connection request)
+		public object Post(Connection request)
 		{
+			HttpListenerRequest iHttpListenerRequest = (HttpListenerRequest)base.RequestContext.Get<IHttpRequest>().OriginalRequest;
+
+			string IP = iHttpListenerRequest.RemoteEndPoint.ToString().Split(':')[0];
+
+			Program.node.childrens.Add (new Tuple<String, Range, Range> (IP + ":" + request.Port.ToString(), new Range(1, 1), new Range(1, 1)));
+			Console.Out.WriteLine (Program.node.childrens.ToJson());
 			return new ConnectionResponse {Primary = new Range(1, 1), Secondary = new Range(1, 1), Data = new List<Tuple<String, String>> ()};
 		}
 	}
